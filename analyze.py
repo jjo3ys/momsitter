8752d28c042097237c2f03f545665175b859268a
jjo3ys <jjo3ys@naver.com>
2022-04-11 14:27:56 +0900

    <feat> get importance on viewcount

--- analyze.py
+++ analyze.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import datetime
 
-from lightgbm import LGBMClassifier, plot_importance, plot_tree
+from lightgbm import LGBMClassifier, plot_importance, LGBMRanker
 
 
 def true_or_false(df):
@@ -17,19 +16,18 @@ df['applyStatus'] = df['applyStatus'].replace(['decline', 'accept'], [0, 1])
 df['userGender'] = df['userGender'].replace(['women', 'man'], [0, 1])
 df['updateDate'] = pd.to_datetime('2021-03-24') - pd.to_datetime(df['updateDate'])
 df['updateDate'] = df['updateDate'].dt.days
-
-
+df['viewCount'] = df['viewCount'].apply(lambda x: 1 if x >  df['viewCount'].mean() else 0)
 lgb = LGBMClassifier(n_estimators=400)
 
-x = df.drop(['userId', 'applyStatus'], axis=1)
-y = df['applyStatus']
+x = df.drop(['userId', 'applyStatus', 'viewCount'], axis=1)
+y = df['viewCount']
 
 train_x = x.iloc[:-1000]
 train_y = y.iloc[:-1000]
 val_x = x.iloc[-1000:]
 val_y = y.iloc[-1000:]
 eval_set = (val_x, val_y)
-
+print(y)
 lgb.fit(x, y, early_stopping_rounds=100, eval_set=eval_set, eval_metric='logloss', verbose=True)
 
 fig, ax = plt.subplots(figsize=(10, 12))
